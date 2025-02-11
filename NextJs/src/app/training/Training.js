6f3d4e2a6ca7832ed58094477cc348a0d83360a4
Master <nghiemxthai@gmail.com>
2025-02-10 22:43:03 -0800

    added data selection option

--- NextJs/src/app/training/Training.js
+++ NextJs/src/app/training/Training.js
@@ -1,11 +1,27 @@
 import './style.css'
 import {Container, Row, Col} from 'react-bootstrap'
+import ListGroup from 'react-bootstrap/ListGroup';
+import {useState} from 'react'
+
 function Training (){
+    const [currentCourse, updateCurrentCourse] = useState(false)
+    const course = [{name: 'Data Analysis'}]
     return(
         <Container fluid className='training'> 
 
-        <h3 className='training-header'> Welcome to Training Center</h3>
+        <div className='training-content'> 
 
+        <h3 className='training-header'> Welcome to Training Center</h3>
+        <h5 className='training-subheader'> Select a Course</h5>
+        <ListGroup>
+      <ListGroup.Item  action  className='course-selection' href="#link1"> Master CSS in 10 hours</ListGroup.Item>
+      <ListGroup.Item action  className='course-selection' href="#link1">Javascript for Beginner</ListGroup.Item>
+      <ListGroup.Item action  className='course-selection' href="#link1"> Sequelize for Beginner </ListGroup.Item>
+      <ListGroup.Item action  className='course-selection' href="#link1">Master Next JS</ListGroup.Item>
+      <ListGroup.Item action  className='course-selection' onClick={()=>alert('clicked')} >Data Analysis</ListGroup.Item>
+
+    </ListGroup>
+        </div>
         </Container>
     )
 }
