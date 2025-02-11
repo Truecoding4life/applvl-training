489a148a2b246a055c6c930aff0c61491eb4428e
Master <nghiemxthai@gmail.com>
2025-02-10 20:30:02 -0800

    added side bar and fixed next js

--- NextJs/src/app/page.js
+++ NextJs/src/app/page.js
@@ -1,18 +1,23 @@
-
+'use client'
 import React from "react";
-import styles from "./style.css";
-import {Container, Row, Col} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+import styles from "./style.css";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 export default function Home() {
   return (
     <div  >
       <main >
-        <Container >
+        <Container fluid>
         <Row>
-            <Col md={3} aria-label="SideBar" style={{ backgroundColor: "#185165" }}>
+            <Col md={2} aria-label="SideBar" className="min-vh-100 d-flex flex-column" style={{ backgroundColor: "#185165" }}>
               <p>AppLevel</p>
             </Col>
-            <Col md={4} aria-label="Content"> Content</Col> 
+            <Col md={10} aria-label="Content" className="min-vh-100 d-flex flex-column"> 
+            <p> Content</p>
+            </Col> 
           </Row>
           
         </Container>
