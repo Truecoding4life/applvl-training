d4da27197f616edd55a58aeacfaba4c9e653da0d
Master <nghiemxthai@gmail.com>
2025-02-11 00:18:09 -0800

    finished adding course selection

--- NextJs/src/app/training/Training.js
+++ NextJs/src/app/training/Training.js
@@ -1,26 +1,50 @@
+'use client'
 import './style.css'
+import 'animate.css';
 import {Container, Row, Col} from 'react-bootstrap'
 import ListGroup from 'react-bootstrap/ListGroup';
 import {useState} from 'react'
+import Alert from 'react-bootstrap/Alert';
+import Button from 'react-bootstrap/Button';
+import { IoAlertCircle } from "react-icons/io5";
 
 function Training (){
+    const [show, setShow] = useState(false);
     const [currentCourse, updateCurrentCourse] = useState(false)
     const course = [{name: 'Data Analysis'}]
+    if(currentCourse){
+        console.log(currentCourse)
+    }
+    const AlertError = ()=> setShow(true)
     return(
         <Container fluid className='training'> 
-
+        {show ? (
+            <Alert className='error-alert animate__animated animate__fadeInDown' style={{ top:20 }} onClose={() => setShow(false)} dismissible>
+        <Alert.Heading> <IoAlertCircle/> Error 404</Alert.Heading>
+        <p>
+          Course not found
+        </p>
+      </Alert>
+    ): null }
         <div className='training-content'> 
-
-        <h3 className='training-header'> Welcome to Training Center</h3>
+        
+        {currentCourse === false ? (
+        <div>
+         <h3 className='training-header'> Welcome to Training Center</h3>
         <h5 className='training-subheader'> Select a Course</h5>
         <ListGroup>
-      <ListGroup.Item  action  className='course-selection' href="#link1"> Master CSS in 10 hours</ListGroup.Item>
-      <ListGroup.Item action  className='course-selection' href="#link1">Javascript for Beginner</ListGroup.Item>
-      <ListGroup.Item action  className='course-selection' href="#link1"> Sequelize for Beginner </ListGroup.Item>
-      <ListGroup.Item action  className='course-selection' href="#link1">Master Next JS</ListGroup.Item>
-      <ListGroup.Item action  className='course-selection' onClick={()=>alert('clicked')} >Data Analysis</ListGroup.Item>
+      <ListGroup.Item action  className='course-selection' onClick={()=>updateCurrentCourse('Data Analysis')}  >Data Analysis</ListGroup.Item>
+      <ListGroup.Item  action  className='course-selection' onClick={AlertError} > Master CSS in 10 hours</ListGroup.Item>
+      <ListGroup.Item action  className='course-selection' onClick={AlertError} >Javascript for Beginner</ListGroup.Item>
+      <ListGroup.Item action  className='course-selection' onClick={AlertError} > Sequelize for Beginner </ListGroup.Item>
+      <ListGroup.Item action  className='course-selection' onClick={AlertError} >Master Next JS</ListGroup.Item>
+    </ListGroup> 
+    </div> ) : (
+        <div>
+            <h3> This is {currentCourse}</h3>
+        </div>
+        )}
 
-    </ListGroup>
         </div>
         </Container>
     )
